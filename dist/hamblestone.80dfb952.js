1997098b2358427eee3690a0bd29f5f0ea401bd7
Michael Hamilton <michaelhamilton626@gmail.com>
2020-11-12 14:10:05 -0500

    style fixes for mobile

--- dist/hamblestone.80dfb952.js
+++ dist/hamblestone.80dfb952.js
@@ -18282,7 +18282,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55644" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60693" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -18459,4 +18459,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","../index.js"], null)
-//# sourceMappingURL=/portfolio-template.80dfb952.js.map
+//# sourceMappingURL=/hamblestone.80dfb952.js.map
